cf29fbbb2503e31d9eb43e205d6ae3b6d33c1eea
Dwayne Williams <dewilliams0327@gmail.com>
2022-07-12 20:46:24 +0000

    change direction on  command

--- include/mecanumbot.hpp
+++ include/mecanumbot.hpp
@@ -15,6 +15,30 @@
 #define MECANUMBOT_DIRECTION_CLOCKWISE 8
 #define MECANUMBOT_DIRECTION_COUNTER_CLOCKWISE 9
 
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+#define UART_TX_PIN 16
+#define UART_RX_PIN 17
+
+#define BRIDGE1_ENA 8
+#define BRIDGE1_IN1 7
+#define BRIDGE1_IN2 6
+
+#define BRIDGE1_ENB 2
+#define BRIDGE1_IN3 4
+#define BRIDGE1_IN4 3
+
+#define BRIDGE2_ENA 10
+#define BRIDGE2_IN1 11
+#define BRIDGE2_IN2 12
+
+#define BRIDGE2_ENB 15
+#define BRIDGE2_IN3 13
+#define BRIDGE2_IN4 14
+
+static int chars_rxed = 0;
+
 class Mecanumbot
 {
 public:
